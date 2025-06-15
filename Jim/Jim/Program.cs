a79d16a756b486dcabdf106ea3096cdd9f36f554
Adrian Sims <adriansims62781@gmail.com>
2025-06-15 12:11:35 +0200

    Updates

---
+++ Jim/Jim/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace Jim;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+        }
+        catch (Exception ex)
+        {
+            var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+            var progname = Path.GetFileNameWithoutExtension(fullname);
+            Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+        }
+    }
+}
